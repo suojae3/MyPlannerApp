f6abdd806e5c1830c53c47901a10ce4085f6d9f0
suojae <suojae3@gmail.com>
2023-08-22 16:18:57 +0900

    Initial Commit

---
+++ MyPlanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlanner
+//
+//  Created by ㅣ on 2023/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
